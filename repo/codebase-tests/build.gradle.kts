a653f005c2a8a29a3d6959f36e19b234ec37bde4
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2023-07-13 11:26:59 +0200

    Update com.fasterxml.jackson.dataformat:jackson-dataformat-xml 2.12.7 -> 2.15.2

--- repo/codebase-tests/build.gradle.kts
+++ repo/codebase-tests/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     testImplementation(intellijCore())
     testImplementation(projectTests(":compiler:tests-common"))
 
-    testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.12.7")
+    testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.15.2")
     testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.7")
     testImplementation("com.fasterxml.woodstox:woodstox-core:6.2.4")
 
