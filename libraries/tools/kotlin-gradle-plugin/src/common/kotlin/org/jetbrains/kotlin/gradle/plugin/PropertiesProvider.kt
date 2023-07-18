4f5df38775eefb2099b1f7869762cef3df81ed7f
Anton Lakotka <anton.lakotka@jetbrains.com>
2023-07-18 10:59:41 +0200

    [Gradle] Add KOTLIN_PERSISTENT_GRADLE_DATA_DIR with `.kotlin/` as default
    Don't store data in `.gradle` as it treated specially by gradle i.e.
    they are exclude this dir from UP-TO-DATE checks. This can lead into
    broken IDE import state which is not fixable by re-importing.
    
    Move CInterop Commonized libraries and Metadata Transformation results
    to new KOTLIN_PERSISTENT_GRADLE_DATA_DIR to avoid IDE import issues.
    
    ^KT-58223

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/PropertiesProvider.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/PropertiesProvider.kt
@@ -550,6 +550,16 @@ internal class PropertiesProvider private constructor(private val project: Proje
         get() = property(PropertyNames.KONAN_DATA_DIR)
 
     /**
+     * The directory where persistent data for Kotlin Gradle Plugin is stored.
+     * This data is not cleaned by gradle clean task. This dir can be used to store data
+     * for IDE import i.e. `./gradlew clean` will not break IDE import state.
+     *
+     * If the property is not set, the default value `$ROOT_DIR/.kotlin` is used.
+     */
+    val kotlinPersistentGradleDataDir: String
+        get() = property(PropertyNames.KOTLIN_PERSISTENT_GRADLE_DATA_DIR) ?: ".kotlin"
+
+    /**
      * Retrieves a comma-separated list of browsers to use when running karma tests for [target]
      * @see KOTLIN_JS_KARMA_BROWSERS
      */
@@ -648,6 +658,7 @@ internal class PropertiesProvider private constructor(private val project: Proje
         val KOTLIN_NATIVE_IGNORE_DISABLED_TARGETS = property("kotlin.native.ignoreDisabledTargets")
         val KOTLIN_NATIVE_SUPPRESS_EXPERIMENTAL_ARTIFACTS_DSL_WARNING = property("kotlin.native.suppressExperimentalArtifactsDslWarning")
         val KONAN_DATA_DIR = property("konan.data.dir")
+        val KOTLIN_PERSISTENT_GRADLE_DATA_DIR = property("kotlin.persistent.gradle.data.dir")
 
         /**
          * Internal properties: builds get big non-suppressible warning when such properties are used
