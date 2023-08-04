4ed9918c36f07a2434f41349bcd5fefc007332c4
Roman Efremov <roman.efremov@jetbrains.com>
2023-08-04 11:33:43 +0200

    [FIR] Store `expectForActual` data for matched members in class
    Currently, it is stored only for source declarations with `actual`
    modifier. But also, this data will be needed for the annotation checker
    in subsequent commits.
    
    ^KT-60668
    ^KT-60936

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/mpp/FirExpectActualMatcherTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/mpp/FirExpectActualMatcherTransformer.kt
@@ -6,20 +6,20 @@
 package org.jetbrains.kotlin.fir.resolve.transformers.mpp
 
 import org.jetbrains.kotlin.config.LanguageFeature
-import org.jetbrains.kotlin.descriptors.ClassKind
+import org.jetbrains.kotlin.fir.FirExpectActualMatchingContext
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.*
-import org.jetbrains.kotlin.fir.declarations.impl.FirPrimaryConstructor
 import org.jetbrains.kotlin.fir.declarations.utils.isActual
-import org.jetbrains.kotlin.fir.declarations.utils.isInline
 import org.jetbrains.kotlin.fir.expectActualMatchingContextFactory
 import org.jetbrains.kotlin.fir.expressions.FirStatement
 import org.jetbrains.kotlin.fir.languageVersionSettings
 import org.jetbrains.kotlin.fir.resolve.ScopeSession
-import org.jetbrains.kotlin.fir.resolve.getContainingClass
 import org.jetbrains.kotlin.fir.resolve.transformers.FirAbstractTreeTransformer
 import org.jetbrains.kotlin.fir.resolve.transformers.FirTransformerBasedResolveProcessor
+import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
 import org.jetbrains.kotlin.fir.visitors.FirTransformer
+import org.jetbrains.kotlin.mpp.DeclarationSymbolMarker
+import org.jetbrains.kotlin.resolve.multiplatform.ExpectActualCompatibility
 
 class FirExpectActualMatcherProcessor(
     session: FirSession,
@@ -40,7 +40,14 @@ open class FirExpectActualMatcherTransformer(
     private val scopeSession: ScopeSession,
 ) : FirAbstractTreeTransformer<Nothing?>(FirResolvePhase.EXPECT_ACTUAL_MATCHING) {
 
-    private val expectActualMatchingContext = session.expectActualMatchingContextFactory.create(session, scopeSession)
+    private val expectActualMatchingContext =
+        object : FirExpectActualMatchingContext by session.expectActualMatchingContextFactory.create(session, scopeSession) {
+
+            override fun onMatchedMembers(expectSymbol: DeclarationSymbolMarker, actualSymbol: DeclarationSymbolMarker) {
+                require(expectSymbol is FirBasedSymbol<*> && actualSymbol is FirBasedSymbol<*>)
+                actualSymbol.fir.expectForActual = mapOf(ExpectActualCompatibility.Compatible to listOf(expectSymbol))
+            }
+        }
 
     // --------------------------- classifiers ---------------------------
     override fun transformTypeAlias(typeAlias: FirTypeAlias, data: Nothing?): FirStatement {
