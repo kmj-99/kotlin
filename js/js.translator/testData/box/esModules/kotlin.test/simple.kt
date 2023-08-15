be6fc024e0f4541a68e983ecf588e191a68d53c6
Artem Kobzar <artem.kobzar@jetbrains.com>
2023-08-15 20:46:14 +0200

    Generate test functions test for per-file

---
+++ js/js.translator/testData/box/esModules/kotlin.test/simple.kt
@@ -0,0 +1,25 @@
+// EXPECTED_REACHABLE_NODES: 1698
+// KJS_WITH_FULL_RUNTIME
+// SKIP_DCE_DRIVEN
+// RUN_UNIT_TESTS
+// ES_MODULES
+
+// FILE: test.kt
+import kotlin.test.Test
+
+class Simple {
+    @Test fun foo() {
+        call("foo")
+    }
+}
+
+// FILE: box.kt
+import common.*
+
+fun box() = checkLog {
+    suite("Simple") {
+        test("foo") {
+            call("foo")
+        }
+    }
+}
