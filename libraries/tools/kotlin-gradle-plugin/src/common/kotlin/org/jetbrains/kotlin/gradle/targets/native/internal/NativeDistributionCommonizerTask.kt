3a42519e8d9a8f2626b18978f6f9317294407f1e
Nataliya.Valtman <nataliya.valtman@jetbrains.com>
2023-08-22 18:51:06 +0200

    fixup! Add language version for KotlinNativeLink tasks' metrics

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/internal/NativeDistributionCommonizerTask.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/internal/NativeDistributionCommonizerTask.kt
@@ -102,7 +102,7 @@ internal abstract class NativeDistributionCommonizerTask
     protected fun run() {
         val metricsReporter = metrics.get()
 
-        addBuildMetricsForTaskAction(metricsReporter = metricsReporter, languageVersion = parseLanguageVersion(customJvmArgs.get())) {
+        addBuildMetricsForTaskAction(metricsReporter = metricsReporter, languageVersion = null) {
             val commonizerRunner = KotlinNativeCommonizerToolRunner(
                 context = KotlinToolRunner.GradleExecutionContext.fromTaskContext(objectFactory, execOperations, logger),
                 settings = runnerSettings.get(),
