7798cfb91306c76bad1f96587c9f11b82d763b0a
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-08-30 21:11:24 +0200

    fixup! [common] avoid usage of SAFE_IDENTIFIER_FOR_NO_NAME

--- compiler/psi/src/org/jetbrains/kotlin/psi/stubs/elements/KtFunctionElementType.java
+++ compiler/psi/src/org/jetbrains/kotlin/psi/stubs/elements/KtFunctionElementType.java
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi.stubs.elements;
@@ -50,7 +39,7 @@ public class KtFunctionElementType extends KtStubElementType<KotlinFunctionStub,
         boolean hasBlockBody = psi.hasBlockBody();
         boolean hasBody = psi.hasBody();
         return new KotlinFunctionStubImpl(
-                (StubElement<?>) parentStub, StringRef.fromString(psi.getName()), isTopLevel, fqName,
+                (StubElement<?>) parentStub, StringRef.fromString(psi.getNameAsSafeName().asString()), isTopLevel, fqName,
                 isExtension, hasBlockBody, hasBody, psi.hasTypeParameterListBeforeFunctionName(),
                 psi.mayHaveContract(),
                 /* contract = */ null,
--- compiler/psi/src/org/jetbrains/kotlin/psi/stubs/elements/KtPropertyElementType.java
+++ compiler/psi/src/org/jetbrains/kotlin/psi/stubs/elements/KtPropertyElementType.java
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi.stubs.elements;
@@ -47,7 +36,7 @@ public class KtPropertyElementType extends KtStubElementType<KotlinPropertyStub,
                               psi.getText(), psi.getParent() != null ? psi.getParent().getText() : "<no parent>");
 
         return new KotlinPropertyStubImpl(
-                (StubElement<?>) parentStub, StringRef.fromString(psi.getName()),
+                (StubElement<?>) parentStub, StringRef.fromString(psi.getNameAsSafeName().asString()),
                 psi.isVar(), psi.isTopLevel(), psi.hasDelegate(),
                 psi.hasDelegateExpression(), psi.hasInitializer(),
                 psi.getReceiverTypeReference() != null, psi.getTypeReference() != null,
