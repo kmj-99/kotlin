e695cac3be3b121f508a7dd6b6071e5a6863eb38
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-08-30 20:59:10 +0200

    [common] avoid usage of SAFE_IDENTIFIER_FOR_NO_NAME
    This name leads to inconsistency between FqName from stub and
    FqName from ClassId.
    The first one is `no_name_in_PSI_3d19d79d_1ba9_4cd0_b7f5_b46aa3cd5d40`
    but the second one is `<no name provided>`
    
    ^KTIJ-26848 Fixed

--- compiler/frontend/src/org/jetbrains/kotlin/resolve/lazy/descriptors/LazyClassDescriptor.java
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/lazy/descriptors/LazyClassDescriptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -666,7 +666,7 @@ public class LazyClassDescriptor extends ClassDescriptorBase implements ClassDes
         ClassConstructorDescriptor constructor = getUnsubstitutedPrimaryConstructor();
         // Don't crash on invalid code.
         InlineClassRepresentation<SimpleType> invalidValueClassRepresentation = new InlineClassRepresentation<>(
-                SpecialNames.SAFE_IDENTIFIER_FOR_NO_NAME, c.getModuleDescriptor().getBuiltIns().getAnyType()
+                SpecialNames.NO_NAME_PROVIDED, c.getModuleDescriptor().getBuiltIns().getAnyType()
         );
         if (constructor == null) {
             return invalidValueClassRepresentation;
